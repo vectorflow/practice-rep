0a7a01aef6ed39e193576f730c033da36609d17d
Aravind <aravindanil314@gmail.com>
2018-11-13 10:01:20 +0000

    node 2nd day

--- Node.js/04.IntroToExpress/FirstExpressApp/app.js
+++ Node.js/04.IntroToExpress/FirstExpressApp/app.js
@@ -4,14 +4,25 @@ var app = express();
 app.get("/", function(req,res){
     res.send("Hi there!!!");
 });
-
 app.get("/bye",function(req,res){
-    res.send("GoodBye");
-})
-
+    res.send("Goodbye!!!");
+});
 app.get("/dog",function(req,res){
     res.send("MEOW!!!");
-})
+});
+app.get("/r/:subredditName",function(req, res) {
+    console.log(req.params);
+    var subreddit=req.params.subredditName;
+    res.send("WELCOME TO THE "+subreddit.toUpperCase()+" SUBREDDIT!!!");
+});
+app.get("/r/:subredditName/comments/:id/:title/",function(req, res) {
+    console.log(req.params);
+    res.send("WELCOME TO THE COMMMENTS PAGE");
+});
+
+app.get("*",function(req,res){
+    res.send("YOU ARE A STAR!!!");
+});
 
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Server has Started !!!");
