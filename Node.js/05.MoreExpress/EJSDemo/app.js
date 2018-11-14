7d1232cc4e063a579f035fed2eda620b3e376db2
Aravind <aravindanil314@gmail.com>
2018-11-14 10:22:26 +0000

    EJS

--- Node.js/05.MoreExpress/EJSDemo/app.js
+++ Node.js/05.MoreExpress/EJSDemo/app.js
@@ -6,7 +6,17 @@ app.get("/",function(req,res){
 });
 app.get("/fallinlovewith/:thing",function(req,res){
     var thing=req.params.thing;
+    
     res.render("love.ejs",{thingVar:thing});
+});
+app.get("/posts",function(req,res){
+    var posts=[
+        {title: "Post 1", author: "Susy"},
+        {title: "My adorable pet bunny", author: "Charlie"},
+        {title: "Can you believe this Pomsky?", author: "Colt"},
+        ];
+    
+    res.render("posts.ejs",{posts: posts});
 });
 
 
